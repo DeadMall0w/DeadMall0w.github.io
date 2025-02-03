524878bd796176fea4d95bd062ace459d930c0c1
Raphael Berquier <raphael.berquier@gmail.com>
2025-02-03 19:18:13 +0100

    Add translations

--- translation.js
+++ translation.js
@@ -6,15 +6,16 @@ async function changeLanguage(lang) {
 
     const changeFile = await fetch("Content/contents.txt");
     const content = await changeFile.text();
-    const changes = content.split("\r\n"); // To remove "retour chariot"
+    const changes = content.split("\n"); // To remove "retour chariot"
     
-    const response = await fetch("Content/"+lang + '.json');
+    const response = await fetch("Content/" + lang + '.json');
     const texts = await response.json();
 
     for (let i = 0; i < changes.length; i++) {
         const name = changes[i];
         try {
             document.getElementById(name).innerHTML = texts[name];
+            //console.log("Changed : " + name);
         } catch (error) {
             console.error("Couldn't find : " + name)
         }
